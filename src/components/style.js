7d63c89891ef5722f0d20879d292a581df5c0ebf
k-737 <kamronalimov80@gmail.com>
2021-11-12 04:17:10 +0500

    body created

--- src/components/style.js
+++ src/components/style.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { ReactComponent as trash } from "../assets/icon/trash2.svg";
+import { ReactComponent as done } from "../assets/icon/done.svg";
 
 export const Container = styled.div`
   margin-top: 50px;
@@ -40,21 +42,73 @@ export const Button = styled.div`
 `;
 
 export const Body = styled.div`
-  font-family: "Courier New", Courier,  bold;
+  font-family: "Courier New", Courier, bold;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   flex-direction: column;
   max-width: 865px;
   min-width: 865px;
-  margin-top: 50px;
+  box-sizing: border-box;
   border-radius: 3px;
-  min-height: fit-content;
+  min-height: 600px;
+  max-height: fit-content;
   background: #fff;
+  padding: 25px;
+  /* padding: ${({ padding }) => padding && `15px`}; */
   border: 1px solid #d1d1cd;
 `;
 
 export const Title = styled.div`
   font-size: 25px;
-  border-bottom: 1px solid #d1d1cd;
+  margin-top: 50px;
+  background: #fff;
+  border: 1px solid #d1d1cd;
+  max-width: 865px;
+  min-width: 865px;
+  border-radius: 3px;
   padding: 15px;
+`;
+Title.Value = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap-reverse;
+  font-size: 19px;
+  border-bottom: 1px solid #d1d1cd;
+  padding: 5px;
+  margin-bottom: 10px;
+  opacity: 0.6;
+`;
+export const Right = styled.div`
+  width: 110px;
+  height: fit-content;
+  display: flex;
+  margin-left: auto;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const IconWrapper = styled.div`
+  box-sizing: border-box;
+  cursor: pointer;
+  width: fit-content;
+  height: fit-content;
+  /* border: 1px solid #d1d1cd; */
+  display: flex;
+  margin-left: auto;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+export const Trash = styled(trash)`
+  width: 22px;
+  height: 22px;
+  fill: #e11300;
+  border-width: 2px;
+`;
+export const Done = styled(done)`
+  width: 22px;
+  height: 22px;
+  fill: green;
+  border-width: 2px;
 `;
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Button,
   Container,
   Input,
   TopSection,
   Title,
-  Wrapper,
   Body,
+  Trash,
+  TrashsWrapper,
+  Done,
+  IconWrapper,
+  Right,
 } from "./style";
 
 export const Todo = () => {
+  const [name, setName] = useState("");
+  const [todo, setTodo] = useState([]);
+  const onAdd = () => {
+    setTodo((prev) => [...prev, { id: prev.length + 1, name: name }]);
+    console.log(todo);
+    setName("");
+  };
+
   return (
     <Container>
       <TopSection>
-        <Input placeholder="What needs to be done" />
-        <Button>Add</Button>
+        <Input
+          placeholder="What needs to be done"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button onClick={onAdd}>Add</Button>
       </TopSection>
+      <Title>Todo List </Title>
       <Body>
-        <Title>Todo List</Title>
+        {todo.map((value) => {
+          return (
+            <Title.Value key={value.id}>
+              {value.name}
+              <Right>
+                <IconWrapper>
+                  <Done />
+                </IconWrapper>
+
+                <IconWrapper>
+                  <Trash />
+                </IconWrapper>
+              </Right>
+            </Title.Value>
+          );
+        })}
       </Body>
     </Container>
   );
